0b4a3c5b8bb5784e61410c5c7a3ceacb3b01990a
Przemysław Tomala <timuslala@gmail.com>
2025-07-08 16:10:15 +0200

    stoi

--- src/main/java/pl/edu/pwr/abis/domain/Aplikacja.java
+++ src/main/java/pl/edu/pwr/abis/domain/Aplikacja.java
@@ -15,13 +15,6 @@ public class Aplikacja{
     @Id
     private Integer numerAplikacji;
     @NotNull
-    @AttributeOverrides({
-        @AttributeOverride(name = "rok", column = @Column(name = "data_zlozenia_rok")),
-        @AttributeOverride(name = "miesiac", column = @Column(name = "data_zlozenia_miesiac")),
-        @AttributeOverride(name = "dzien", column = @Column(name = "data_zlozenia_dzien"))
-    })
-    private Data dataZlozenia;
-    @NotNull
     private StanAplikacji stan;
     @Column(nullable = true)
     private String powodOdrzucenia;
@@ -46,41 +39,14 @@ public class Aplikacja{
     private Boolean czyZgodnyZDefinicja;
     @NotNull
     private Boolean czyUkonczony;
-    @Column(nullable = true)
-    @AttributeOverrides({
-        @AttributeOverride(name = "rok", column = @Column(name = "data_zakonczenia_rok")),
-        @AttributeOverride(name = "miesiac", column = @Column(name = "data_zakonczenia_miesiac")),
-        @AttributeOverride(name = "dzien", column = @Column(name = "data_zakonczenia_dzien"))
-    })
-    private Data dataZakonczenia;
+
     @Column(nullable = true)
     private static FazaOceny fazaOceny;
     private Double sredniaOcenaJury;
-    @ManyToOne()
-    private Osoba osoba;
-    @OneToOne(optional = true, cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    private Plik listReferencyjny;
-    @OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    private Plik[] zdjecia;
-    @OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    private Plik[] logotypy;
-    @OneToOne(optional = true, cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    private Plik film;
-    @OneToOne(optional = true, cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    private Plik oswiadczenieAplikanta;
-    @OneToOne(optional = true, cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    private Plik oswiadczenieInicjatora;
-    @OneToMany(mappedBy = "aplikacja", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    private Pytanie[] pytanie;
-    @OneToOne(optional = true, mappedBy = "aplikacja")
-    private RaportAplikacyjny[] raportAplikacyjny;
-    @ManyToOne()
-    private Organizacja organizacja;
+
     private Boolean czyOrganizacjaInicjujaca;
     @ManyToOne()
     private KategoriaProjektu kategoriaProjektu;
-    @ManyToMany()
-    private Set<PrzydzialAsesora> przydzieleniAsesorowie;
-    @ManyToOne(optional = true)
-    private PrzydzialAsesora assesorWidacy;
+    @ManyToOne()
+    private EdycjaKonkursu edycjaKonkursu;
 }
--- src/main/java/pl/edu/pwr/abis/domain/EdycjaKonkursu.java
+++ src/main/java/pl/edu/pwr/abis/domain/EdycjaKonkursu.java
@@ -20,28 +20,9 @@ public class EdycjaKonkursu{
     private Integer minCzasTrwaniaProjektu;
     private Integer minLiczbaPodwykonawcow;
     private Integer maxCzasDoUkonczeniaProjektuMiesiace;
-    @AttributeOverrides({
-        @AttributeOverride(name = "rok", column = @Column(name = "planowany_rok")),
-        @AttributeOverride(name = "miesiac", column = @Column(name = "planowany_miesiac")),
-        @AttributeOverride(name = "dzien", column = @Column(name = "planowany_dzien"))
-    })
-    private Data planowanaDataZlozeniaIO;
-    @ManyToMany()
-    private Set<CzlonekBiuraNagrody> skladBiuraNagrody;
-    @ManyToOne(optional = true)
-    private CzlonekBiuraNagrody przewodniczacyBiuraNagrody;
-    @ManyToMany()
-    private Set<Jury> skladJury;
-    @ManyToOne(optional = true)
-    private Jury przewodniczacyJury;
-    @OneToOne(optional = true, mappedBy = "edycjaKonkursu", cascade = CascadeType.REMOVE)
-    private Plik listaFinalistow;
-    //@OneToOne(optional = true, mappedBy = "edycjaKonkursu", cascade = CascadeType.REMOVE)
-    private static Plik regulamin;
-    @OneToOne(optional = true, mappedBy = "edycjaKonkursu", cascade = CascadeType.REMOVE)
-    private Plik wyniki;
     @OneToMany(mappedBy = "edycjaKonkursu")
-    private Set<Wydarzenie> harmonogram;
+    private Set<Aplikacja> aplikacje;
+
     @ManyToMany()
     private Set<KategoriaProjektu> kategorieProjektow;
 }
